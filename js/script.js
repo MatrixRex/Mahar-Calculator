fb66176dfc7e78e751f88e4e9d6896f7778329c6
MatrixRex <matrixrex05@gmail.com>
2025-11-30 09:02:10 +0600

    removed price from main, update js to point to seperated data

--- js/script.js
+++ js/script.js
@@ -23,7 +23,8 @@ themeToggle.addEventListener('click', () => {
 // Data Fetching & Calculation
 async function fetchPrice() {
     try {
-        const response = await fetch('./price.json');
+       
+        const response = await fetch('https://raw.githubusercontent.com/matrixrex/Mahar-Calculator/data/price.json');
         if (!response.ok) throw new Error('Price data not found');
         
         const data = await response.json();
